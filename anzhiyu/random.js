02c3a083519452eda92365ddaba51c30ff566449
Fly1216 <3099058643@qq.com>
2023-05-27 10:07:38 +0800

    Site updated: 2023-05-27 10:07:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/05/17/Azkaban学习文档/","2021/12/10/可视化手册/","2023/02/27/读写HBase/","2023/03/12/电商例题文档/","2023/05/25/国赛试题技术点/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/05/17/Azkaban学习文档/","2023/02/27/读写HBase/","2023/05/25/国赛试题技术点/","2023/03/12/电商例题文档/","2021/12/10/可视化手册/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
